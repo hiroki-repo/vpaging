eed85b1dc77b4f532ae0370dfe9214c4d27eafba
hiroki-repo <hiroki7v11@gmail.com>
2022-05-22 15:43:59 +0900

    Add files via upload

--- vpageing/dllmain.cpp
+++ vpageing/dllmain.cpp
@@ -27,10 +27,20 @@ int (*pagehandler)(int);
 extern "C" {
 #endif
 
+    void (*cb4chstat)(unsigned int new_fc);
+
+    UINT32 typemask = ~0;
+    UINT32 nfcbuf = 0;
+
     __declspec(dllexport) void memaccessptrset(UINT32* tmp, int tmp2) { memaccesstmp = tmp; pagesize = tmp2; }
     __declspec(dllexport) void setmemaccess4nonmac(int (*tmp)(UINT32, int, int)) { memaccesses = tmp; }
     __declspec(dllexport) void setpagehandler(int (*tmp)(int)) { pagehandler = tmp; }
-    __declspec(dllexport) int memaccessmod(UINT32 addr, int val, int type) { switch (type) { case 0: if ((pagehandler != 0) && (memaccesstmp[addr / pagesize] == 0)) { if (pagehandler(addr) == 0) { return 0; } else if (pagehandler(addr) == 1) { return memaccesses(addr, val, type); } } tempmem[memaccesstmp[addr / pagesize] + (addr % pagesize)] = val; return 0; case 1: if ((pagehandler != 0) && (memaccesstmp[addr / pagesize] == 0)) { if (pagehandler(addr) == 0) { return 0; } else if (pagehandler(addr) == 1) { return memaccesses(addr, val, type); } } return tempmem[memaccesstmp[addr / pagesize] + (addr % pagesize)]; default: return memaccesses(addr, val, type); } }
+    __declspec(dllexport) int memaccessmod(UINT32 addr, int val, int type) { switch (type&typemask) { case 0: if ((pagehandler != 0) && (memaccesstmp[addr / pagesize] == 0)) { if (pagehandler(addr) == 0) { return 0; } else if (pagehandler(addr) == 1) { return memaccesses(addr, val, type); } } tempmem[memaccesstmp[addr / pagesize] + (addr % pagesize)] = val; return 0; case 1: if ((pagehandler != 0) && (memaccesstmp[addr / pagesize] == 0)) { if (pagehandler(addr) == 0) { return 0; } else if (pagehandler(addr) == 1) { return memaccesses(addr, val, type); } } return tempmem[memaccesstmp[addr / pagesize] + (addr % pagesize)]; default: return memaccesses(addr, val, type); } }
+    __declspec(dllexport) void settypemask(UINT32 tm){ typemask = tm; }
+    __declspec(dllexport) UINT32 gettypemask() { return typemask; }
+    __declspec(dllexport) void m68knfccallback(unsigned int new_fc) { if ((nfcbuf & 4) != (new_fc & 4)) { cb4chstat(new_fc); } nfcbuf = new_fc; }
+    __declspec(dllexport) UINT32 m68kgetnfc() { return nfcbuf; }
+    __declspec(dllexport) void m68ksetcb4getpriv(void(*cb4chstattemp)(unsigned int)) { cb4chstat = cb4chstattemp; }
 
 #ifdef __cplusplus
 }
